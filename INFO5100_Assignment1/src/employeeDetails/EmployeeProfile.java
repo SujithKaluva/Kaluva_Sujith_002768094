4a19d4d8d5a5453a9ae0eb3aab337b845b936f94
Sujith Kaluva <sujithkaluva1999@gmail.com>
2022-09-29 19:59:37 -0400

    upload 2

---
+++ INFO5100_Assignment1/src/employeeDetails/EmployeeProfile.java
@@ -0,0 +1,117 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+package employeeDetails;
+
+/*
+ *
+ *
+ * @author sujithkaluva
+ */
+
+public class EmployeeProfile {
+    
+    private String empName;
+    private String empId;
+    private String empAge;
+    private String empGender;
+    private String startDate;
+    private String level;
+    private String teamInfo;
+    private String positionTitle;
+    private String cellNumber;
+    private String emailId;
+    private byte[] photo;
+
+    public String getEmpName() {
+        return empName;
+    }
+
+    public void setEmpName(String empName) {
+        this.empName = empName;
+    }
+
+    public String getEmpId() {
+        return empId;
+    }
+
+    public void setEmpId(String empId) {
+        this.empId = empId;
+    }
+
+    public String getEmpAge() {
+        return empAge;
+    }
+
+    public void setEmpAge(String empAge) {
+        this.empAge = empAge;
+    }
+
+    public String getEmpGender() {
+        return empGender;
+    }
+
+    public void setEmpGender(String empGender) {
+        this.empGender = empGender;
+    }
+
+    public String getStartDate() {
+        return startDate;
+    }
+
+    public void setStartDate(String startDate) {
+        this.startDate = startDate;
+    }
+
+    public String getLevel() {
+        return level;
+    }
+
+    public void setLevel(String level) {
+        this.level = level;
+    }
+
+    public String getTeamInfo() {
+        return teamInfo;
+    }
+
+    public void setTeamInfo(String teamInfo) {
+        this.teamInfo = teamInfo;
+    }
+
+    public String getPositionTitle() {
+        return positionTitle;
+    }
+
+    public void setPositionTitle(String positionTitle) {
+        this.positionTitle = positionTitle;
+    }
+
+    public String getCellNumber() {
+        return cellNumber;
+    }
+
+    public void setCellNumber(String cellNumber) {
+        this.cellNumber = cellNumber;
+    }
+
+    public String getEmailId() {
+        return emailId;
+    }
+
+    public void setEmailId(String emailId) {
+        this.emailId = emailId;
+    }
+
+    public byte[] getPhoto() {
+        return photo;
+    }
+
+    public void setPhoto(byte[] photo) {
+        this.photo = photo;
+    }
+    
+    
+}
